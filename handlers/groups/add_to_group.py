77eea5c27740177535bf27e699eadbf6885bf05c
Pendosv <djvlad967891@gmail.com>
2021-09-12 13:26:51 +0300

    add some

--- handlers/groups/add_to_group.py
+++ handlers/groups/add_to_group.py
@@ -5,5 +5,6 @@ from utils.misc.formatter import get_formatted_message
 
 @dp.my_chat_member_handler()
 async def add_bot_to_group(my_chat_member: types.ChatMemberUpdated):
-    await bot.send_message(my_chat_member.from_user.id, get_formatted_message(my_chat_member),
+    await bot.send_message(my_chat_member.from_user.id,
+                           get_formatted_message(my_chat_member),
                            parse_mode=types.ParseMode.HTML)
