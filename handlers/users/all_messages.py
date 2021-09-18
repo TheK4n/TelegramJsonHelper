aa9dfbfa5e4e8c88142f19bc2617567897894d3b
Pendosv <djvlad967891@gmail.com>
2021-09-18 14:05:42 +0300

    add some

--- handlers/users/all_messages.py
+++ handlers/users/all_messages.py
@@ -2,10 +2,11 @@ from aiogram import types
 from loader import dp
 from utils.misc.formatter import get_formatted_message
 
+content_types = ['text', 'document', 'audio', 'photo', 'sticker', 'video', 'video_note',
+                 'voice', 'contact', 'dice', 'poll', 'venue', 'animation', 'location']
 
-@dp.edited_message_handler()
-@dp.message_handler(
-    content_types=['text', 'document', 'audio', 'photo', 'sticker', 'video', 'video_note',
-                   'voice', 'contact', 'dice', 'poll', 'venue', 'animation', 'location'])
+
+@dp.edited_message_handler(content_types=content_types)
+@dp.message_handler(content_types=content_types)
 async def all_messages_handler(message: types.Message):
     await message.reply(get_formatted_message(message), parse_mode=types.ParseMode.HTML)
